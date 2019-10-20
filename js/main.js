7b682229e534fe2c4cb43d4de410acbf6dc64583
Zoya <zoyakrasnopolskaa@gmail.com>
2019-10-20 21:39:38 +0300

    correct

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ $(function() {
   $( "#tabs" ).tabs({ classes: { "ui-tabs-panel": "row" } });
   $( "#tabs2" ).tabs();
   $('.single-item').slick({
+    prevArrow: '\
+      <div class="prev-arrow">\
+        <span><</span>\
+        <button type="button" class="slick-prev"></button>\
+      </div>\
+    ',
+    nextArrow: '\
+      <div class="next-arrow">\
+        <span>></span>\
+        <button type="button" class="slick-next"></button>\
+      </div>\
+    '
   });
   $('.section-open').on('click', function(e){
     e.preventDefault();
